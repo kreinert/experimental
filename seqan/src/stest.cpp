5b85f6c26b7a2cf2402e73da04cbc86431548cba
kreinert <knut.reinert@fu-berlin.de>
2015-08-24 23:59:48 +0200

    added makefiles

---
+++ seqan/src/stest.cpp
@@ -0,0 +1,22 @@
+#include <seqan/basic.h>
+#include <seqan/sequence.h>
+#include <seqan/stream.h>
+#include <iostream>
+#include <omp.h>
+
+using namespace std;
+
+
+int main()
+{
+    
+#pragma omp parallel
+    {
+#pragma omp critical
+        {
+            printf("Hello OpenMP from thread %i\n", omp_get_thread_num());
+        }
+    }
+    return 0;
+}
+
